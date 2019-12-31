a632e0d74c255eb3e76a6e93a503377fe9bbb44a
DANIEL74164 <jhondanielcc@gmail.com>
2019-12-31 00:44:17 -0500

    Agregando portada

--- application/views/user_portada.php
+++ application/views/user_portada.php
@@ -5,7 +5,7 @@
         <div class="row mt">
             <div class="col-md-12">
                 <div class="content-panel">
-                        
+                    <!--    
                     <table class="table table-hover">
                         
                         <thead>
@@ -15,24 +15,54 @@
                                 <th><i class="fa fa-calendar"></i> FECHA</th>
                                 <th><i class=" fa fa-edit"></i> OBSERVACION</th>
                                 <th><i class=" fa fa-edit"></i> OPCIONES</th>
-                              -->
+                             
                             </tr>
                         </thead>
                       
-                            <tbody>
+                            <tbody>-->
+                              <table class="table table-hover">
+                              <?php 
+                              $prueba=1;
+                              foreach ($ejemplares as $tipo) { ?>
+                                
+                                  <thead>
+                                    <tr>
+                                      <th><?php echo "Area de ".$tipo->categoria ?></th>
+                                        
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <td>
+                                        
+                                        <img src="<?php echo base_url().'data/'.$tipo->ejem_portada?>" alt="Image placeholder" class="card-img-top" height="132" width="93">
+                                      
+                                      </td>
+                                    </tr>
+                                    <tr>
+                                      <td><?php echo $tipo->ejem_titulo ?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>
+                                        <?php $prueba=$tipo->ejem_valoracion;
+                                          while ($prueba>0) {
+                                             echo '<i class=" fa fa-star"></i>';
+                                             $prueba-=1;
+                                           } ?>
+                                      </td>
+                                    </tr> 
+                                  </tbody>
+                                <?php
+                                
+                                if ($tipo->ejem_cate_id != $prueba) {
+                                  $prueba+=1;
+                                } 
 
-                              <?php foreach ($ejemplares as $tipo) { ?>
-                                <?php echo $temp=$tipo->categoria ?>
-                                <?php echo $tipo->ejem_cate_id; ?>
-                                <?php if ($temp==$tipo->ejem_cate_id) {
-                                  echo "hola";
-                                } ?>
-
-                              <?php } ?>
+                               } ?>
+                            </table>
 
 
-
-
+                                <!--
 
                                  <?php foreach ($ejemplares as $peticion){?>
                                     <tr>
@@ -49,7 +79,7 @@
                               </tbody>
                           </table>
 
-
+-->
 
                       </div>
                   </div>
